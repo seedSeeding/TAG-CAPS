6d32771d4c295edac0eae979065954dccb6d10ac
seed <yellyhaze000@gmail.com>
2024-11-23 02:26:55 +0800

    hosting update 3

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+    build: {
+        manifest: true,
+        outDir: 'public/build', 
+    },
     plugins: [
         react({include: /\.(js|jsx|ts|tsx)$/}),
+        
         laravel({
             input: ['resources/css/app.css','resources/js/script.js', 'resources/js/app.jsx'],
             refresh: true,
